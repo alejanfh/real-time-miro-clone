800f2d562b82b6f7b40555a6287995529a6d4455
Alejandro Fortes Hidalgo <afortes@eunoia.es>
2024-02-10 12:03:03 +0100

    Now you can favorite and unfavorite cards

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -9,6 +9,10 @@ import { Footer } from './footer'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Actions } from '@/components/actions'
 import { MoreHorizontal } from 'lucide-react'
+import { useApiMutation } from '@/hooks/use-api-mutation'
+import { api } from '@/convex/_generated/api'
+import { toast } from 'sonner'
+import { useMutation } from 'convex/react'
 
 type BoardCardProps = {
   id: string
@@ -36,6 +40,24 @@ export const BoardCard = ({
     addSuffix: true,
   })
 
+  // const habdleFavorite = useMutation(api.board.favorite)
+  // const habdleUnfavorite = useMutation(api.board.unfavorite)
+
+  const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(
+    api.board.favorite
+  )
+  const { mutate: unFavorite, pending: pendingUnfavorite } = useApiMutation(
+    api.board.unfavorite
+  )
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      unFavorite({ id }).catch(() => toast.error('Failed to unfavorite'))
+    } else {
+      onFavorite({ id, orgId }).catch(() => toast.error('Failed to favorite'))
+    }
+  }
+
   return (
     <Link href={`/board/${id}`}>
       <div className='group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden'>
@@ -53,8 +75,8 @@ export const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => console.log('')}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnfavorite}
         />
       </div>
     </Link>
