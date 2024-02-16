b5a9c3f149009ec994f4834dc20a4dba8fd8415e
Alejandro Fortes Hidalgo <afortes@eunoia.es>
2024-02-16 15:08:38 +0100

    Add layer inserting rectangle into the canvas

--- components/room.tsx
+++ components/room.tsx
@@ -3,6 +3,8 @@
 import { ReactNode } from 'react'
 import { RoomProvider } from '@/liveblocks.config'
 import { ClientSideSuspense } from '@liveblocks/react'
+import { LiveList, LiveMap, LiveObject } from '@liveblocks/client'
+import { Layer } from '@/types/canvas'
 
 type RoomProms = {
   children: ReactNode
@@ -12,7 +14,14 @@ type RoomProms = {
 
 export const Room = ({ children, roomId, fallback }: RoomProms) => {
   return (
-    <RoomProvider id={roomId} initialPresence={{ cursor: null }}>
+    <RoomProvider
+      id={roomId}
+      initialPresence={{ cursor: null, selection: [] }}
+      initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList(),
+      }}
+    >
       <ClientSideSuspense fallback={fallback}>
         {() => children}
       </ClientSideSuspense>
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,17 +1,18 @@
-import { createClient } from '@liveblocks/client'
+import { createClient, LiveList, LiveMap, LiveObject } from '@liveblocks/client'
 import { createRoomContext } from '@liveblocks/react'
+import { Layer, Color } from './types/canvas'
 
 const client = createClient({
   authEndpoint: '/api/liveblocks-auth',
-  throttle: 16
+  throttle: 16,
 })
 
 // Presence represents the properties that exist on every user in the Room
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  cursor: { x: number, y: number } | null,
-  // ...
+  cursor: { x: number; y: number } | null
+  selection: string[]
 }
 
 // Optionally, Storage represents the shared document that persists in the
@@ -19,19 +20,19 @@ type Presence = {
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
 type Storage = {
-  // author: LiveObject<{ firstName: string, lastName: string }>,
-  // ...
+  layers: LiveMap<string, LiveObject<Layer>>
+  layerIds: LiveList<string>
 }
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  id?: string,  
+  id?: string
   info?: {
-    name?: string,
+    name?: string
     picture?: string
-  },  
+  }
 }
 
 // Optionally, the type of custom events broadcast and listened to in this
